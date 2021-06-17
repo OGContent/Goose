f878e405c16eb357cfae4b63b2284deb2c0d68c3
OGContent <81373802+OGContent@users.noreply.github.com>
2021-06-17 10:32:31 +0300

    new cmd

--- src/main/java/com/github/ogcontent/Main.java
+++ src/main/java/com/github/ogcontent/Main.java
@@ -1,19 +1,21 @@
 package com.github.ogcontent;
 
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
 
 public final class Main extends JavaPlugin {
     private static Main instance;
-
     public static Main getInstance() {
         return instance;
     }
     @Override
     public void onEnable() {
         instance = this;
-        getServer().getPluginManager().registerEvents(new Event(), this); }
-
+        getServer().getPluginManager().registerEvents(new Event(), this);
+        Objects.requireNonNull(getCommand("goose")).setExecutor(new VersionCommand());
+    }
     @Override
     public void onDisable() {
         instance = null;
---
+++ src/main/java/com/github/ogcontent/VersionCommand.java
@@ -0,0 +1,16 @@
+package com.github.ogcontent;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class VersionCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) { //If you have any suggestion on this file, make a suggestion or a pull request and i'll look into it
+        if (args[0].equalsIgnoreCase("version")) sender.sendMessage(ChatColor.GREEN + "Goose version: 1.0");
+        else { sender.sendMessage(ChatColor.GREEN + "Goose will take over the world, just wait."); }
+        return true;
+    }
+}
