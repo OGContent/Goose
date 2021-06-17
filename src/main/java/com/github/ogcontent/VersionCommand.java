f2d336e8ea19f01584a25b984ee3660ba944ed20
OGContent <81373802+OGContent@users.noreply.github.com>
2021-06-17 11:10:44 +0300

    edit

--- src/main/java/com/github/ogcontent/VersionCommand.java
+++ src/main/java/com/github/ogcontent/VersionCommand.java
@@ -9,8 +9,13 @@ import org.jetbrains.annotations.NotNull;
 public class VersionCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) { //If you have any suggestion on this file, make a suggestion or a pull request and i'll look into it
-        if (args[0].equalsIgnoreCase("version")) sender.sendMessage(ChatColor.GREEN + "Goose version: 1.1");
-        else { sender.sendMessage(ChatColor.GREEN + "Goose will take over the world, just wait."); }
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.AQUA + "I WILL TAKE OVER THE WORLD, HAHAHAHA" + " oh and you can only do /goose version for now ");
+            return true;
+        } else if (args[0].equalsIgnoreCase("version")) {
+            sender.sendMessage(ChatColor.GREEN + "Goose version: 1.1");
+            return true;
+        }
         return true;
     }
 }
