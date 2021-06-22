ffadb8fdf121de61cac33b3d47e484f048a9bd31
OGContent <81373802+OGContent@users.noreply.github.com>
2021-06-22 17:48:03 +0300

    quick api change

--- src/main/java/com/github/ogcontent/Event.java
+++ src/main/java/com/github/ogcontent/Event.java
@@ -1,25 +1,35 @@
 package com.github.ogcontent;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreedEvent;
+
+
+import org.bukkit.event.player.PlayerMoveEvent;
+import ru.beykerykt.lightapi.LightAPI;
+
+import java.util.Objects;
 
 // why is this here
 
 
 public class Event implements Listener {
-
     @EventHandler
     public void Breed4(EntityBreedEvent e) {
         if (e.getFather().getType().equals(EntityType.CHICKEN)) {
             Entity child = e.getEntity();
             child.setInvulnerable(true);
-            child.getLocation().createExplosion(10, false);
+            Location exactLoc = child.getLocation();
+            Objects.requireNonNull(exactLoc.getWorld()).createExplosion(exactLoc, 10, true);
             child.setCustomName("Goose");
-            if (!child.getLocation().add(0, 5, 0).getBlock().isSolid()) {
-                child.teleport(child.getLocation().add(0, 5, 0));
+            if (!exactLoc.add(0, 5, 0).getBlock().getType().isSolid()) {
+                child.teleport(exactLoc.add(0, 5, 0));
             }
             child.setInvulnerable(false);
         }
